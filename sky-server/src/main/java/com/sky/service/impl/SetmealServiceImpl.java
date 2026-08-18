1dbbd3de184d8b6f8196f937302d34ef9cbd810b
yonghao liu <2078901436@qq.com>
2026-08-18 15:30:48 +0800

    day7.2 & 7.3 通过springCache利用注解缓存的功能 缓存套餐

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -148,6 +148,7 @@ public class SetmealServiceImpl implements SetmealService {
      * @return
      */
     public List<Setmeal> list(Setmeal setmeal){
+
         return setmealMapper.list(setmeal) ;
     }
 
